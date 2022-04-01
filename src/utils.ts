eaceb8753072c05bb81ea4722eee48b44e385d11
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-01 01:53:04 +0100

    refactor: simplify commands

--- src/utils.ts
+++ src/utils.ts
@@ -13,17 +13,3 @@ export const makeFileExecutable = async (file: string) => {
     process.start()
   })
 }
-
-export function wrapCommand(
-  // eslint-disable-next-line no-unused-vars
-  command: (...args: any[]) => void | Promise<void>
-  // eslint-disable-next-line no-unused-vars
-): (...args: any[]) => void {
-  return async function wrapped(...args: any[]) {
-    try {
-      await command(...args)
-    } catch (err) {
-      nova.workspace.showErrorMessage("command not implemented")
-    }
-  }
-}
